9f1b3d291fdae89bdb145d99ff0dd0dac0d6f488
CattleHorse <cattle_horse@qq.com>
2026-05-28 13:35:58 +0800

    ci: 发布油猴脚本 meta 更新文件

--- build-userscript.js
+++ build-userscript.js
@@ -1,3 +1,4 @@
+import { writeFileSync } from 'node:fs';
 import esbuild from 'esbuild';
 
 const version = (() => {
@@ -7,7 +8,25 @@ const version = (() => {
     return `${d.getUTCFullYear()}.${pad(d.getUTCMonth() + 1)}.${pad(d.getUTCDate())}.${pad(d.getUTCHours())}${pad(d.getUTCMinutes())}`;
 })();
 const downloadURL = process.env.DOWNLOAD_URL || 'https://github.com/Cattle0Horse/link-cleaner/releases/latest/download/link-cleaner.user.js';
-const updateURL = process.env.UPDATE_URL || downloadURL;
+const updateURL = process.env.UPDATE_URL || 'https://github.com/Cattle0Horse/link-cleaner/releases/latest/download/link-cleaner.meta.js';
+const userscriptHeader = `
+    // ==UserScript==
+    // @name        Link Cleaner
+    // @version     ${version}
+    // @author      Cattle0Horse
+    // @description 清洗网页上带有各种跟踪参数的链接
+    // @source      https://github.com/Cattle0Horse/link-cleaner
+    // @downloadURL ${downloadURL}
+    // @updateURL   ${updateURL}
+    // @match       *://*/*
+    // @connect     *
+    // @grant       GM_getValue
+    // @grant       GM_registerMenuCommand
+    // @grant       GM_setClipboard
+    // @grant       GM_setValue
+    // @grant       GM_xmlhttpRequest
+    // ==/UserScript==
+`.trim().split('\n').map(e => e.trim()).join('\n');
 
 esbuild.buildSync({
     entryPoints: ['src/entry-userscript.js'],
@@ -16,24 +35,8 @@ esbuild.buildSync({
     bundle: true,
     minify: true,
     banner: {
-        js: `
-            // ==UserScript==
-            // @name        Link Cleaner
-            // @version     ${version}
-            // @author      Cattle0Horse
-            // @description 清洗网页上带有各种跟踪参数的链接
-            // @source      https://github.com/Cattle0Horse/link-cleaner
-            // @downloadURL ${downloadURL}
-            // @updateURL   ${updateURL}
-            // @match       *://*/*
-            // @connect     *
-            // @grant       GM_getValue
-            // @grant       GM_registerMenuCommand
-            // @grant       GM_setClipboard
-            // @grant       GM_setValue
-            // @grant       GM_xmlhttpRequest
-            // ==/UserScript==
-            /* eslint-disable */
-        `.trim().split('\n').map(e => e.trim()).join('\n'),
+        js: `${userscriptHeader}\n/* eslint-disable */`,
     },
 });
+
+writeFileSync('dist/link-cleaner.meta.js', `${userscriptHeader}\n`);
